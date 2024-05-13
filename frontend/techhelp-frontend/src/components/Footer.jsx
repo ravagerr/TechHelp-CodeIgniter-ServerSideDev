189d46a68a057d3f55e254e08712f1aa51b05e41
ravagerr <04topper-clink@icloud.com>
2024-05-13 01:05:17 +0100

    header and footer

--- frontend/techhelp-frontend/src/components/Footer.jsx
+++ frontend/techhelp-frontend/src/components/Footer.jsx
@@ -1,6 +1,12 @@
 export default function Footer() {
     return (
-        <p>Footer</p>
+        <footer>
+            <p className="footer-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem corrupti veritatis neque veniam cupiditate ad esse debitis, totam deserunt est amet vel commodi, vero eveniet excepturi dolore minima aut aliquam.</p>
+            <div className="footer-items">
+                <p>TechHelp</p>
+                <p>United Kingdom</p>
+            </div>
+        </footer>
     )
 
 }
--- frontend/techhelp-frontend/src/components/Header.jsx
+++ frontend/techhelp-frontend/src/components/Header.jsx
@@ -8,14 +8,20 @@ export default function Header() {
 
     return (
         <header>
-            <p>Header</p>
-            {user ? (
-                <button>
-                    <Link to={`/profile/${user.id}`}>Profile</Link>
-                </button>
-            ) : (
-                <p>Not logged in</p>
-            )}
+            <nav>
+                <Link to={'/'} className='nav-link nav-index'>TechHelp</Link>
+                <div className="nav-right">
+                    <Link to={'/'} className='nav-link'>Browse</Link>
+                    <Link to={'/'} className='nav-link'>Search</Link>
+                    {user ? (
+                        <button>
+                            <Link to={`/profile/${user.id}`}>Profile</Link>
+                        </button>
+                    ) : (
+                        <p>Not logged in</p>
+                    )}
+                </div>
+            </nav>
         </header>
     )
 }
